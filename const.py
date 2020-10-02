d128c66f70cbb808fd2bfcff168b0bb20602f3af
Romain DESSART <romaindessart@gmail.com>
2020-10-02 17:10:44 +0200

    moved the MAZE_DEFAULT_OBJ to package const

--- const.py
+++ const.py
@@ -4,12 +4,11 @@
 """Program constant"""
 
 from maze_data.maze_object import MazeObject
+from maze_data.const import MAZE_DEFAULT_OBJ
 
 
 DEFAULT_MAZE_FILE = "./resources/levels/level0.lvl"
 
-MAZE_DEFAULT_OBJ = ["Empty", "Wall", "Guard", "Needle", "Plastic Tube",
-                    "Ether", "MacGyver"]
 
 OBJECTS = [MazeObject(MAZE_DEFAULT_OBJ.index("Ether"), None),
            MazeObject(MAZE_DEFAULT_OBJ.index("Needle"), None),
