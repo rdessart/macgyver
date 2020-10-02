b3e3b59bf7079469633620dbed61ffe55e9e5e2a
Romain DESSART <romaindessart@gmail.com>
2020-10-02 17:10:26 +0200

    created a package constant

---
+++ maze_data/const.py
@@ -0,0 +1,7 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""Package constant"""
+
+MAZE_DEFAULT_OBJ = ["Empty", "Wall", "Guard", "Needle", "Plastic Tube",
+                    "Ether", "MacGyver"]
