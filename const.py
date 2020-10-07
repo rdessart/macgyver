88290f199c7ba19eba6e647e56761b949dfeac8e
Romain DESSART <romaindessart@gmail.com>
2020-10-07 17:24:26 +0200

    Added constant value, corrected typo

--- const.py
+++ const.py
@@ -7,18 +7,24 @@ import os
 
 IMG_FOLDER = os.path.join(os.path.dirname(__file__), "resources", "img")
 
+# Item dictionnary key is the id of object.
+# Value is a tuple of (Name, Image file | or None | Cropping data if require)
 MAZE_OBJ = {0: ("Empty", None),
             1: ("Wall", "floor-tiles-20x20.png", (20, 20, 220, 0)),
             2: ("Guard", "Gardien.png"),
             3: ("Needle", "aiguille.png"),
-            4: ("Plastic Tube","tube_plastique.png"),
+            4: ("Plastic Tube", "tube_plastique.png"),
             5: ("Ether", "ether.png"),
             6: ("MacGyver", "MacGyver.png")}
 
 MAZE_PLACABLE = [3, 4, 5]
 
+MAZE_BLOCKING = [1]
+
 DEFAULT_MAZE_FILE = "./resources/levels/level0.lvl"
 
-DEFAULT_INPUT_MSG = "Select your action [Z/S/Q/D]: "
+SPRITE_SIZE = (30, 30)
 
-RESOLUTION = [700, 1000]
+MAZE_SIZE = (15, 15)
+
+RESOLUTION = (MAZE_SIZE[0] * SPRITE_SIZE[0], MAZE_SIZE[1] * SPRITE_SIZE[1])
