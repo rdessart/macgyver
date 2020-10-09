611b3e552464ad8b103e1dba293f8ab8bea04c5a
Romain DESSART <romaindessart@gmail.com>
2020-10-09 15:16:09 +0200

    added syringe object

--- const.py
+++ const.py
@@ -15,7 +15,8 @@ MAZE_OBJ = {0: ("Empty", None),
             3: ("Needle", "aiguille.png"),
             4: ("Plastic Tube", "tube_plastique.png"),
             5: ("Ether", "ether.png"),
-            6: ("MacGyver", "MacGyver.png")}
+            6: ("MacGyver", "MacGyver.png"),
+            7: ("Syringe", "seringue.png")}
 
 MAZE_PLACABLE = [3, 4, 5]
 
@@ -25,6 +26,16 @@ DEFAULT_MAZE_FILE = "./resources/levels/level0.lvl"
 
 SPRITE_SIZE = (30, 30)
 
-MAZE_SIZE = (15, 15)
+RESOLUTION = (450, 510)
 
-RESOLUTION = (MAZE_SIZE[0] * SPRITE_SIZE[0], MAZE_SIZE[1] * SPRITE_SIZE[1])
+INTRO_TEXT = [("MacGyver: The game!", 35, 0),
+              ("Oh no, MacGyver is in trouble!", 20, 40),
+              ("Help him by finding the exit.", 20, 60),
+              ("But wait, there is a guard...", 20, 80),
+              ("Fortunately some items are hidden in the maze:", 20, 100),
+              ("- A plastic tube", 20, 120),
+              ("- A bit of ether", 20, 140),
+              ("- A needle", 20, 160),
+              ("You need to find them order to craft a syringe.", 20, 180),
+              ("This will allow McGyver to knock out the guard", 20, 200),
+              ("Press ENTER to start ! ", 30, 250)]
