c2759e797f58ef928fd0705ce35264fc6ebace8c
Romain DESSART <romaindessart@gmail.com>
2020-10-10 22:49:10 +0200

    reorganised value; set debug as True

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ import game
 def main(args: list):
     """Program entry point"""
     filepath = "./resources/levels/level0.lvl"  # Default value
-    debug = False
     if len(args) > 2:
         filepath = os.path.join(os.path.dirname(__file__),
                                 args[2])
+    debug = True
     play = True
     while play:
         the_game = game.Game(filepath)
