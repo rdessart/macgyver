0a3fad43e5602659dae85e387d7d5c2103ba5e8f
Romain DESSART <romaindessart@gmail.com>
2020-10-20 15:15:51 +0200

    corrected ui display of welcome message

--- const.py
+++ const.py
@@ -29,13 +29,13 @@ SPRITE_SIZE = (30, 30)
 RESOLUTION = (450, 510)
 
 INTRO_TEXT = [("MacGyver: The game!", 35, 0),
-              ("Oh no, MacGyver is in trouble!", 20, 40),
-              ("Help him by finding the exit.", 20, 60),
-              ("But wait, there is a guard...", 20, 80),
-              ("Fortunately some items are hidden in the maze:", 20, 100),
-              ("- A plastic tube", 20, 120),
-              ("- A bit of ether", 20, 140),
-              ("- A needle", 20, 160),
-              ("You need to find them order to craft a syringe.", 20, 180),
-              ("This will allow McGyver to knock out the guard", 20, 200),
-              ("Press ENTER to start ! ", 30, 250)]
+              ("Oh no, MacGyver is in trouble!", 20, 45),
+              ("Help him by finding the exit.", 20, 70),
+              ("But wait, there is a guard...", 20, 95),
+              ("Fortunately some items are hidden in the maze:", 20, 120),
+              ("- A plastic tube", 20, 145),
+              ("- A bit of ether", 20, 165),
+              ("- A needle", 20, 185),
+              ("You need to find them order to craft a syringe.", 20, 215),
+              ("This will allow McGyver to knock out the guard", 20, 235),
+              ("Press ENTER to start ! ", 30, 255)]
